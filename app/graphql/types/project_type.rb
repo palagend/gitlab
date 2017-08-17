050774a6cd3e886427bdccf66c7af527a49be75f
Nick Thomas <nick@gitlab.com>
2017-08-17 19:14:33 +0100

    Support more project attributes

--- app/graphql/types/project_type.rb
+++ app/graphql/types/project_type.rb
@@ -1,8 +1,6 @@
 Types::ProjectType = GraphQL::ObjectType.define do
   name 'Project'
 
-  ## BasicProjectDetails entity
-
   field :id, !types.ID
 
   field :full_path, !types.ID
@@ -11,7 +9,7 @@ Types::ProjectType = GraphQL::ObjectType.define do
   field :name_with_namespace, types.String
   field :name, types.String
 
-  field :descripton, types.String
+  field :description, types.String
 
   field :default_branch, types.String
   field :tag_list, types.String
@@ -28,44 +26,39 @@ Types::ProjectType = GraphQL::ObjectType.define do
 
   field :archived, types.Boolean
 
-  # TODO(nick.thomas)
-  # expose :visibility # -> field :visibility, types.VisibilityLevel
-  # expose :owner, using: Entities::UserBasic, unless: ->(project, options) { project.group }
+  field :visibility, types.String # TODO(nick.thomas): -> field :visibility, types.VisibilityLevel
+
+  # TODO(nick.thomas): Should we expose bare IDs any more?
+  field :creator_id, types.ID
 
   field :container_registry_enabled, types.Boolean
   field :shared_runners_enabled, types.Boolean
   field :lfs_enabled, types.Boolean
 
-=begin
-  # TODO(nick.thomas): convery all these
-
-  # Expose old field names with the new permissions methods to keep API compatible
-  expose(:issues_enabled) { |project, options| project.feature_available?(:issues, options[:current_user]) }
-  expose(:merge_requests_enabled) { |project, options| project.feature_available?(:merge_requests, options[:current_user]) }
-  expose(:wiki_enabled) { |project, options| project.feature_available?(:wiki, options[:current_user]) }
-  expose(:jobs_enabled) { |project, options| project.feature_available?(:builds, options[:current_user]) }
-  expose(:snippets_enabled) { |project, options| project.feature_available?(:snippets, options[:current_user]) }
-
-  expose :creator_id
-  expose :namespace, using: 'API::Entities::Namespace'
-  expose :forked_from_project, using: Entities::BasicProjectDetails, if: lambda { |project, options| project.forked? }
-
-  # How do we do `if` in graphql?
+  # TODO(nick.thomas): what is user_can_admin_project ?
   # expose :import_error, if: lambda { |_project, options| options[:user_can_admin_project] }
-  expose :open_issues_count, if: lambda { |project, options| project.feature_available?(:issues, options[:current_user]) }
-  expose :runners_token, if: lambda { |_project, options| options[:user_can_admin_project] }
-  expose :statistics, using: 'API::Entities::ProjectStatistics', if: :statistics
+  # expose :runners_token, if: lambda { |_project, options| options[:user_can_admin_project] }
 
-  expose :avatar_url do |user, options|
-    user.avatar_url(only_path: false)
+  # TODO(nick.thomas): which user is this?
+  # expose :avatar_url do |user, options|
+  #   user.avatar_url(only_path: false)
+  # end
+
+  %i[issues merge_requests wiki snippets].each do |feature|
+    field "#{feature}_enabled", types.Boolean do
+      resolve -> (project, args, ctx) { project.feature_available?(feature, ctx['current_user']) }
+    end
   end
 
-  expose :shared_with_groups do |project, options|
-    SharedGroup.represent(project.project_group_links.all, options)
+  field :jobs_enabled, types.Boolean do
+    resolve -> (project, args, ctx) { project.feature_available?(:builds, ctx['current_user']) }
   end
 
-  expose :public_builds, as: :public_jobs
-=end
+  field :public_jobs, types.Boolean, property: :public_builds
+
+  field :open_issues_count, types.Int do
+    resolve -> (project, args, ctx) { project.open_issues_count if project.feature_available?(:issues, ctx['current_user']) }
+  end
 
   field :import_status, types.String
   field :ci_config_path, types.String
@@ -74,6 +67,15 @@ Types::ProjectType = GraphQL::ObjectType.define do
   field :request_access_enabled, types.Boolean
   field :only_allow_merge_if_all_discussions_are_resolved, types.Boolean
   field :printing_merge_request_link_enabled, types.Boolean
+
+  # TODO(nick.thomas): associations we don't support yet
+  # expose :owner, using: Entities::UserBasic, unless: ->(project, options) { project.group }
+  # expose :statistics, using: 'API::Entities::ProjectStatistics', if: :statistics
+  # expose :namespace, using: 'API::Entities::Namespace'
+  # expose :forked_from_project, using: Entities::BasicProjectDetails, if: lambda { |project, options| project.forked? }
+  # expose :shared_with_groups do |project, options|
+  #   SharedGroup.represent(project.project_group_links.all, options)
+  # end
 
   field :merge_requests, types[Types::MergeRequestType] do
     argument :iid, types[types.ID]
