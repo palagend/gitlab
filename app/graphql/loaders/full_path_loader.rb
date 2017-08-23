042a86363e59dc407bada496de321db557878bce
Nick Thomas <nick@gitlab.com>
2017-08-23 11:19:35 +0100

    Avoid an N+1 in Loaders::FullPathLoader

--- app/graphql/loaders/full_path_loader.rb
+++ app/graphql/loaders/full_path_loader.rb
@@ -6,7 +6,8 @@ class Loaders::FullPathLoader < Loaders::BaseLoader
   end
 
   def perform(keys)
-    relation = model.where_full_path_in(keys)
+    # `with_route` prevents relation.all.map(&:full_path)` from being N+1
+    relation = model.where_full_path_in(keys).with_route
     fulfill_all(relation, keys) { |instance| instance.full_path }
   end
 end
